ddf10463af4c959d544a532673b4036b06ed7806
Dan Finch <d@nfin.ch>
2014-01-06 04:58:19 -0800

    Added RequestScope

--- src/Trichome.Web/RequestScope.cs
+++ src/Trichome.Web/RequestScope.cs
@@ -1,13 +1,38 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Trichome.Web {
     public class RequestScope : IScope {
-        public object Resolve(Type type, Creator constructor) {
-            throw new NotImplementedException();
+        private const string HTTP_CONTEXT_KEY = "Trichome.Web.RequestScope";
+
+        public object Resolve(Type type, Creator creator) {
+            var context = HttpContext.Current;
+            var instances = context.Items[HTTP_CONTEXT_KEY]
+                as ConcurrentDictionary<Type, object>;
+            if (instances == null) {
+                instances = new ConcurrentDictionary<Type, object>();
+                context.Items.Add(HTTP_CONTEXT_KEY, instances);
+                context.AddOnRequestCompleted(DisposeRequest);
+            }
+            return instances.GetOrAdd(type, t => creator.Create());
+        }
+
+        private void DisposeRequest(HttpContext context) {
+            var instances = context.Items[HTTP_CONTEXT_KEY]
+                as ConcurrentDictionary<Type, object>;
+            foreach (var reference in instances.Values) {
+                var disposable = reference as IDisposable;
+                if (disposable != null) {
+                    disposable.Dispose();
+                }
+            }
+            instances.Clear();
+            context.Items.Remove(HTTP_CONTEXT_KEY);
         }
 
         public void Dispose() { }
